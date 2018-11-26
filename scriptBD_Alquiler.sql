61b151b27524ae2a857d44327a4a0788e99d054c
Monitor <jesuscanales@outllok.com>
2018-11-26 09:23:02 -0500

    He creado la base de datos Alquiler

--- scriptBD_Alquiler.sql
+++ scriptBD_Alquiler.sql
@@ -4,4 +4,11 @@ show databases;
 /* Eliminar la base de datos Alquiler */
 drop database miAlquiler;
 
-/* */
+/* Crear nuestra base de datos Alquiler teniendo en cuenta la configuración de caracteres */
+create database dbAlquiler default character set utf8;
+
+/* Poner en uso la base de datos */
+use dbAlquiler;
+
+/* Verificar que la base de datos esté en uso*/
+select database();
